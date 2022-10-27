88cc694cf31cf6c858f5a78fa93ce9c29aac8ae1
Jakub Choroszucha <jakub.rrk@outlook.com>
2022-10-27 16:37:01 +0200

    add button number display

--- script.js
+++ script.js
@@ -15,4 +15,31 @@ let operate = (num1, num2, operator) => {
   }
 }
 
+let display = document.querySelector("#display");
+display.textContent = "999";
 
+let button1 = document.querySelector("#button1");
+let button2 = document.querySelector("#button2");
+let button3 = document.querySelector("#button3");
+let button4 = document.querySelector("#button4");
+let button5 = document.querySelector("#button5");
+let button6 = document.querySelector("#button6");
+let button7 = document.querySelector("#button7");
+let button8 = document.querySelector("#button8");
+let button9 = document.querySelector("#button9");
+let buttonPlus = document.querySelector("#buttonPlus");
+let buttonMinus = document.querySelector("#buttonMinus");
+let buttonMultiply = document.querySelector("#buttonMultiply");
+let buttonDivide = document.querySelector("#buttonDivide");
+let buttonC = document.querySelector("#buttonC");
+let buttonEquals = document.querySelector("#buttonEquals");
+
+button1.addEventListener("click", () => {display.textContent += "1";});
+button2.addEventListener("click", () => {display.textContent += "2";});
+button3.addEventListener("click", () => {display.textContent += "3";});
+button4.addEventListener("click", () => {display.textContent += "4";});
+button5.addEventListener("click", () => {display.textContent += "5";});
+button6.addEventListener("click", () => {display.textContent += "6";});
+button7.addEventListener("click", () => {display.textContent += "7";});
+button8.addEventListener("click", () => {display.textContent += "8";});
+button9.addEventListener("click", () => {display.textContent += "9";});
