e7e193988321d246d52d84e1d0ed8c5339ea4527
Jakub Choroszucha <jakub.rrk@outlook.com>
2022-10-27 15:30:31 +0200

    add files js functions

---
+++ script.js
@@ -0,0 +1,18 @@
+let add = (num1, num2) => num1 + num2;
+let subtract = (num1, num2) => num1 - num2;
+let multiply = (num1, num2) => num1 * num2;
+let divide = (num1, num2) => num1 / num2;
+
+let operate = (num1, num2, operator) => {
+  switch (operator) {
+    case "+": return add(num1, num2);
+    case "-": return subtract(num1, num2);
+    case "*": return multiply(num1, num2);
+    case "/": return divide(num1, num2);
+    default: {
+      console.log("Wrong operator");
+    }
+  }
+}
+
+
