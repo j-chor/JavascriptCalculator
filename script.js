5ebb920742d46af6584ee44fa5c68615d5315fb0
Jakub Choroszucha <jakub.rrk@outlook.com>
2022-10-27 19:25:53 +0200

    working calculator

--- script.js
+++ script.js
@@ -15,6 +15,13 @@ let operate = (num1, num2, operator) => {
   }
 }
 
+
+let clear = () => {
+  num1 = 0;
+  num2 = 0;
+  display.textContent = "";
+ };
+
 let display = document.querySelector("#display");
 display.textContent = "999";
 
@@ -34,12 +41,42 @@ let buttonDivide = document.querySelector("#buttonDivide");
 let buttonC = document.querySelector("#buttonC");
 let buttonEquals = document.querySelector("#buttonEquals");
 
-button1.addEventListener("click", () => {display.textContent += "1";});
-button2.addEventListener("click", () => {display.textContent += "2";});
-button3.addEventListener("click", () => {display.textContent += "3";});
-button4.addEventListener("click", () => {display.textContent += "4";});
-button5.addEventListener("click", () => {display.textContent += "5";});
-button6.addEventListener("click", () => {display.textContent += "6";});
-button7.addEventListener("click", () => {display.textContent += "7";});
-button8.addEventListener("click", () => {display.textContent += "8";});
-button9.addEventListener("click", () => {display.textContent += "9";});
+button1.addEventListener("click", () => { display.textContent += "1"; });
+button2.addEventListener("click", () => { display.textContent += "2"; });
+button3.addEventListener("click", () => { display.textContent += "3"; });
+button4.addEventListener("click", () => { display.textContent += "4"; });
+button5.addEventListener("click", () => { display.textContent += "5"; });
+button6.addEventListener("click", () => { display.textContent += "6"; });
+button7.addEventListener("click", () => { display.textContent += "7"; });
+button8.addEventListener("click", () => { display.textContent += "8"; });
+button9.addEventListener("click", () => { display.textContent += "9"; });
+
+buttonC.addEventListener("click", () => { clear(); });
+
+let num1;
+let num2;
+let operator;
+buttonPlus.addEventListener("click", () => {
+  num1 = display.textContent;
+  operator = "+";
+  display.textContent = "";
+});
+buttonMinus.addEventListener("click", () => {
+  num1 = display.textContent;
+  operator = "-";
+  display.textContent = "";
+});
+buttonMultiply.addEventListener("click", () => {
+  num1 = display.textContent;
+  operator = "*";
+  display.textContent = "";
+});
+buttonDivide.addEventListener("click", () => {
+  num1 = display.textContent;
+  operator = "/";
+  display.textContent = "";
+});
+buttonEquals.addEventListener("click", () => {
+  num2 = display.textContent;
+  display.textContent = operate(num1, num2, operator);
+})
